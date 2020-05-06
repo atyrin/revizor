be9f74ba45291639425baa9eff09f8107004cbac
atyrin <tyrin.andrew@gmail.com>
2020-05-06 10:58:09 +0200

    hide not implemented reports

--- src/WebUI/Workspace/Reports.ts
+++ src/WebUI/Workspace/Reports.ts
@@ -43,7 +43,7 @@ export const REPORTS: Array<Report> = [
         displayName: "Managed Disks not connected to VM",
         query: "where type =~ 'microsoft.compute/disks' and isempty(managedBy) | project id, name, resourceGroup | sort by resourceGroup asc",
         type: AzResourceTypes.ManagedDisk
-    },
+    }/*,
     {
         key: "AboundedAvailabilitySet",
         displayName: "Abounded Availability Set"
@@ -59,5 +59,5 @@ export const REPORTS: Array<Report> = [
     {
         key: "ExpiredAdApplications",
         displayName: "Expired Active Directory Applications"
-    }
+    }*/
 ]
